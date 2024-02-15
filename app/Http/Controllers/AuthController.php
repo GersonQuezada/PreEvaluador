17b065324f8cc59148843b909cf3b720f7ae7935
GersonQuezada <gerson.quezada@manuela.org.pe>
2024-02-14 23:38:42 -0500

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function login(Request $request){
+        $this->validate($request,[
+            'login' => 'required',
+            'pswd' => 'required'
+        ],[
+            'required' => 'El campo :attribute es requerido para autenticacion del usuario.'
+        ]);
+
+        $user = User::where('login', $request->login)->where('pswd',$request->pswd)->first();
+
+        if(!$user){
+            return response()->json(['error' => 'Unauthorized','message' => 'Usuario no registrado'], 401);
+        }
+        $this->GenerateToken($user);
+
+        return  response()->json($this->GenerateToken($user), 200);
+
+    }
+    protected function GenerateToken($user){
+        $key = env('JWT_KEY');
+        $payload = [
+            'host' => 'http://credimujer.com',
+            'Sub-Dominio' => 'http://aplativos.credimujer.com',
+            'create_in' => time(),
+            'expires_in' => time() * 60,
+            'User_Auth' => $user->email
+        ];
+        $jwt = JWT::encode($payload, $key, 'HS256');
+        // return response()->json(['error' => 'Unauthorized','message' => 'Usuario no registrado'], 401);
+
+
+        return  [
+            'access_token' => $jwt,
+            'token_type' => 'bearer',
+            'expires_in' =>  date('d/M/Y H:i:s',$payload['expires_in'])
+        ];
+        // $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+    }
+}
